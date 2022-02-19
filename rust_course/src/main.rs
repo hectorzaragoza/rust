15c8605a77b6d95f5f95a3bb2208eb5e63662208
hectorzaragoza <zaragoza09@gmail.com>
2022-02-18 19:27:11 -0600

    structs

--- rust_course/src/main.rs
+++ rust_course/src/main.rs
@@ -3,12 +3,24 @@ struct Point {
     x: f64,
     y: f64
 }
+
+struct Line {
+    start: Point,
+    end: Point
+}
+
 // Structures can use structures
 fn structures() {
     let p = Point {
         x: 3.0, y: 4.0
     };
     println!("Point p is at ({},{})", p.x, p.y);
+    let p2 = Point {
+        x: 5.0, y: 10.0
+    };
+
+    let myline = Line {start: p, end: p2};
+    println!("This is my line, it uses structs within a struct: {}", myline);
 }
 
 fn main() {
